c8a3516857ff18620a07cd52fd14ba3c3c295f2d
MihalyPapp <mispaaa@gmail.com>
2021-02-02 22:41:00 +0100

    format comment's createdDate value

--- src/main/java/com/mihalypapp/redditclone/dto/CommentDto.java
+++ src/main/java/com/mihalypapp/redditclone/dto/CommentDto.java
@@ -2,6 +2,8 @@ package com.mihalypapp.redditclone.dto;
 
 import java.time.Instant;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,6 +17,7 @@ public class CommentDto {
 	private String text;
 	private Long postId;
 	private String userName;
+	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "UTC")
 	private Instant createdDate;
 	
 }
